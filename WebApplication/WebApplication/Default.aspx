d3d66a7f8e3d55469a8b7771b9d1f2c66ae190a6
daniailmas <daniailmas@gmail.com>
2016-01-21 17:04:47 +0500

    Code

---
+++ WebApplication/WebApplication/Default.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Tasks</title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <div class="col-lg-11 col-lg-offset-1">
+                <h1>Task List Manager</h1>
+                <h3>Click on an item to delete it.
+                    Click on insert button to insert new Item. 
+                    Click delete all button to remove all tasks in list.
+                </h3>
+                <div class="container-fluid">
+                    <h4>Default Task List:</h4>
+                    <asp:BulletedList ID="bltasklist" runat="server" CssClass="list-group">
+                    </asp:BulletedList>
+                    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#insert">Insert Task</button>
+                    <button type="button" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#delete">Delete Task</button>
+                    <asp:Button ID="btndeleteall" Text="Delete All Tasks" runat="server" OnClick="btndeleteall_Click" CssClass="btn btn-danger btn-lg" />
+                </div>
+
+            </div>
+        </div>
+    
+
+    <!-- Insert Modal -->
+    <div id="insert" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Insert Task</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Enter Task name below:</p>
+                    <div class="col-lg-4">
+                        <asp:TextBox ID="txttitle" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <asp:Button ID="btninsert" Text="Insert" runat="server" OnClick="btninsert_Click" CssClass="btn btn-primary" />
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+     <!-- Delete Modal -->
+    <div id="delete" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Delete Task</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Enter Task name below:</p>
+                    <div class="col-lg-4">
+                        <asp:TextBox ID="txtdeltitle" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <asp:Button ID="btndelete"  Text="Delete Task" runat="server" CssClass="btn btn-danger" OnClick="btndelete_Click" />
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    </form>
+
+    <script src="Scripts/jquery-1.9.1.min.js"></script>
+    <script src="Scripts/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ WebApplication/WebApplication/Default.aspx.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Tasks.v1;
+using Google.Apis.Tasks.v1.Data;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+
+namespace WebApplication
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private string title = "Default";
+        private TasksService service = new TasksService();
+        private TaskList list = new TaskList();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+                //User Credentional Authorization
+                UserCredential credential;
+                using (var stream = new FileStream(Server.MapPath("/client_secret.json"), FileMode.Open, FileAccess.Read))
+                {
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        new[] { TasksService.Scope.Tasks },
+                        "user", CancellationToken.None, new FileDataStore("Tasks.Auth.Store")).Result;
+                }
+
+                // Create the service.
+                service = new TasksService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Tasks App",
+                });
+          
+            //Check if default task list exist.
+            // If yes show else create and show
+
+            if (!ListExists(service, title))
+            {
+                //Create
+                CreateTasklist(service);
+                //Show
+                ShowList(service);
+
+            }
+            else
+            {
+                //Show
+                ShowList(service);
+            }
+            list = service.Tasklists.List().Execute().Items.Where(l => l.Title == "Default").SingleOrDefault();
+        }
+  
+        private bool ListExists(TasksService service, string title)
+        {
+            return (from TaskList taskList in service.Tasklists.List().Execute().Items
+                    where taskList.Title == title
+                    select taskList).Count() > 0;
+        }
+
+        private void ShowList(TasksService service)
+        {
+            bltasklist.Items.Clear();
+            TaskList list = service.Tasklists.List().Execute().Items.Where(l => l.Title == "Default").SingleOrDefault();
+            var tasks = service.Tasks.List(list.Id).Execute();
+            if(tasks.Items!= null)
+            {
+                foreach (var item in tasks.Items)
+                {
+                    if (item.Title != null)
+                    {
+                        bltasklist.Items.Add(item.Title);
+                    }
+
+                }
+            }
+            else
+            {
+                bltasklist.Items.Add("List Is Empty!");
+            }
+            
+            
+        }
+
+        private void CreateTasklist(TasksService service)
+        {
+            var list = new TaskList();
+            list.Title = title;
+            list = service.Tasklists.Insert(list).Execute();
+        }
+
+        protected void btninsert_Click(object sender, EventArgs e)
+        {
+            if(txttitle.Text!="")
+            {
+                string t = txttitle.Text;
+                Task task = new Task();
+                task.Title = t;
+                service.Tasks.Insert(task, list.Id).Execute();
+                ShowList(service);
+            }
+        }
+
+        protected void btndeleteall_Click(object sender, EventArgs e)
+        {
+            var tasks = service.Tasks.List(list.Id).Execute();
+            foreach (var item in tasks.Items)
+            {
+                service.Tasks.Delete(list.Id, item.Id).Execute();
+            }
+            ShowList(service);
+        }
+
+        protected void btndelete_Click(object sender, EventArgs e)
+        {
+            if(txtdeltitle.Text!="")
+            {
+                string t = txtdeltitle.Text;
+                Task task = service.Tasks.List(list.Id).Execute().Items.Where(l => l.Title == t).SingleOrDefault();
+                if (task != null)
+                {
+                    service.Tasks.Delete(list.Id, task.Id).Execute();
+                    ShowList(service);
+
+                }
+                else
+                {
+                    Console.WriteLine("No task of this title");
+                }
+            }
+        }
+    }
+}
